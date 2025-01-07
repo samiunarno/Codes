26703e4afec5fa4d0d7ec0dd43d1c3f06c1465ed
samiunarno <samiunarno.iub@gmail.com>
2025-01-07 23:38:46 +0600

    pr

--- new.py
+++ new.py
@@ -1,2 +1,18 @@
+class student:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+    def welcome(self):
+        print("Welcome to Portal")
+    def names (self):
+        print(self.name)        
+    def get_marks(self):
+        return self.marks    
+    
 
-print("Hello World")
+
+
+s = student("Karan",90)
+s.welcome()
+print(s.names())
+print(s.get_marks())
